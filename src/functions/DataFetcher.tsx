aca948bed314f0b667db6f890800eb9e95c4d0e1
Aepino01 <aepino@espol.edu.ec>
2025-07-09 15:48:50 +0000

    Cambio de datos por ciudad

--- src/functions/DataFetcher.tsx
+++ src/functions/DataFetcher.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import type { OpenMeteoResponse } from "../types/DashboardTypes";
 
 const DataFetcher = () => {
