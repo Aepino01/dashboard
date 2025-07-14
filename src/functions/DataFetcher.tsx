aab826972959a0f83b29ab73ddc9632e499185e0
Aepino01 <aepino@espol.edu.ec>
2025-07-14 15:56:26 +0000

    ActividadenCLase

--- src/functions/DataFetcher.tsx
+++ src/functions/DataFetcher.tsx
@@ -11,6 +11,8 @@ const coordenadasPorCiudad: Record<string, { latitude: number; longitude: number
   Cuenca: { latitude: -2.9005, longitude: -78.9897 },
 };
 
+const CACHE_MINUTES = 10;
+
 const DataFetcher = ({city}:Props) => {
   const [data, setData] = useState<OpenMeteoResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
@@ -22,6 +24,26 @@ const DataFetcher = ({city}:Props) => {
     const url =
       `https://api.open-meteo.com/v1/forecast?latitude=${coord.latitude}&longitude=${coord.longitude}&hourly=temperature_2m,wind_speed_10m&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m`;
 
+    const datakey=`weather_${city}`;
+    const cached= localStorage.getItem(datakey);
+    let cacheData:{ data: OpenMeteoResponse; timestamp: number } | null = null;
+    if (cached) {
+      try {
+        cacheData = JSON.parse(cached);
+      } catch (e) {
+        console.error("Error al parsear el cache:", e);
+      }
+    }
+
+    const now = Date.now();
+    const isCacheValid = cacheData && (now - cacheData.timestamp < CACHE_MINUTES * 60 * 1000;
+    
+    if (isCacheValid) {
+      setData(cacheData!.data);
+      setLoading(false);
+      return;
+    }
+
     const fetchData = async () => {
       try {
         const response = await fetch(url);
@@ -30,7 +52,9 @@ const DataFetcher = ({city}:Props) => {
         }
         const result: OpenMeteoResponse = await response.json();
         setData(result);
+        localStorage.setItem(datakey, JSON.stringify({ data: result, timestamp: now }));
       } catch (err: any) {
+        setData(cacheData!.data);
         setError(err.message || "Error desconocido");
       } finally {
         setLoading(false);
