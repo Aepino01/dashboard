23b86308ed943052159769c7ee37378c367f7d1d
Aepino01 <aepino@espol.edu.ec>
2025-07-16 15:37:48 +0000

    h

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "21199adf659a9b979572e74b6e36502f"
   }, {
     "url": "index.html",
-    "revision": "0.lbeitbcmoh"
+    "revision": "0.cm3i3rkth4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
